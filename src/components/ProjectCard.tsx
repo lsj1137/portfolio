b7f217e1b72a142d1bb54d7ae3da4759677d1ff7
LimSeJun <lsj1137@naver.com>
2025-06-01 22:56:56 +0900

    [Feat, Fix] 프로젝트 카드 상세 내용을 호버했을때 나타나도록 수정

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export default function ProjectCard({
   photo,
@@ -15,20 +16,27 @@ export default function ProjectCard({
   url: string;
   skills: string[];
 }) {
+  const [hovering, setHovering] = useState(false);
+
   return (
     <div
-      className="flex flex-col cursor-pointer"
+      className=" relative flex flex-col cursor-pointer transition-all ease-in-out duration-200"
       onClick={() => window.open(url, "_blank")}
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
     >
-      <Image src={photo} alt="" width={400} height={150} />
-      <div className="flex flex-col bg-white rounded-b-[20px] h-[200px] shadow p-5">
-        <p lang="ko" className="text-[18px] mb-4">
+      <Image
+        src={photo}
+        alt=""
+        width={400}
+        height={150}
+        className="rounded-t-[20px]"
+      />
+      <div className="flex flex-col bg-white rounded-b-[20px] h-[150px] shadow p-5">
+        <p lang="ko" className="flex-1 text-[18px] mb-4">
           {title}
         </p>
-        <p lang="ko" className="text-[16px] flex-1">
-          {content}
-        </p>
-        <div className="flex gap-5">
+        <div className="flex gap-5 justify-end">
           {skills.map((skill, i) => (
             <Image
               key={i}
@@ -39,6 +47,18 @@ export default function ProjectCard({
             />
           ))}
         </div>
+      </div>
+      <div
+        className={` absolute flex flex-col gap-10 px-5 justify-center items-center h-full w-full text-center rounded-[20px] bg-white/90 transition-all ease-in-out duration-200 ${
+          hovering ? "opacity-100" : " opacity-0"
+        }`}
+      >
+        <p lang="ko" className="text-[16px]">
+          {content}
+        </p>
+        <p lang="ko" className="decoration-1 decoration-black">
+          자세히 보기 🔗
+        </p>
       </div>
     </div>
   );
